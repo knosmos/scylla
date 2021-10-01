74251b43e29a17420295a87470e318193af34325
unknown <knosmos@gmail.com>
2021-09-30 21:03:40 -0400

    initial commit

---
+++ boat.js
@@ -0,0 +1,113 @@
+let c = 11;
+let cw = window.innerWidth/c;
+
+let rightPressed, leftPressed, downPressed, upPressed;
+let mx = 0;
+let my = 0;
+
+let gameOver = false;
+
+class Boat {
+    constructor() {
+        this.elem = document.getElementById("boat");
+        this.elem.style.width = `${cw*0.75}px`;
+        this.x = 100;
+        this.y = window.innerHeight/2;
+        this.speed = 4;
+    }
+    move() {
+        // Keyboard Control
+        if (rightPressed) {
+            this.x += this.speed;
+            this.elem.style.transform = "translate(-50%, -50%) scaleX(1)";
+        }
+        if (leftPressed) {
+            this.x -= this.speed;
+            this.elem.style.transform = "translate(-50%, -50%) scaleX(-1)";
+        }
+        if (upPressed) {
+            this.y -= this.speed;
+        }
+        if (downPressed) {
+            this.y += this.speed;
+        }
+
+
+        // Mouse Control
+        /*
+        // Find the direction vector, then normalize
+        let dx = mx-this.x;
+        let dy = my-this.y;
+        let vecLength = Math.sqrt(dx*dx + dy*dy + 0.01);
+        let vec = [dx/vecLength, dy/vecLength];
+        // console.log(mx, my, dx, dy, this.x, this.y, vecLength, vec);
+
+        if (vecLength > 3) {
+            this.x += vec[0]*this.speed;
+            this.y += vec[1]*this.speed;
+        }
+        */
+
+        this.x = Math.max(0, Math.min(window.innerWidth, this.x));
+        this.y = Math.max(0, Math.min(window.innerHeight, this.y));
+
+        this.elem.style.left = `${this.x}px`;
+        this.elem.style.top = `${this.y}px`;
+
+        if (!gameOver) {
+            requestAnimationFrame(this.move.bind(this));
+        }
+        
+    }
+}
+
+function keyDownHandler(event) {
+    if(event.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(event.keyCode == 37) {
+        leftPressed = true;
+    }
+    if(event.keyCode == 40) {
+    	downPressed = true;
+    }
+    else if(event.keyCode == 38) {
+    	upPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if(event.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(event.keyCode == 37) {
+        leftPressed = false;
+    }
+    if(event.keyCode == 40) {
+    	downPressed = false;
+    }
+    else if(event.keyCode == 38) {
+    	upPressed = false;
+    }
+}
+
+onmousemove = function(e){
+    mx = e.clientX;
+    my = e.clientY;
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+let boat = new Boat();
+boat.move();
+
+let playing = false;
+onkeydown = function(e){
+    if (!playing) {
+        let music = new Audio("assets/music.mp3");
+        music.loop = true;
+        music.play();
+        playing = true;        
+    }
+}
---
+++ obstacles.js
@@ -0,0 +1,163 @@
+let scrollSpeed = 2;
+let score = 0;
+
+class obj{
+    constructor(img, x, y){
+        this.elem = document.createElement("img");
+        this.elem.className = "obj";
+        this.elem.src = img;
+        this.elem.style.width = `${cw*0.75}px`;
+        this.x = x;
+        this.y = y;
+        
+        document.getElementById("obstacles").appendChild(this.elem);
+    }
+
+    boatOverlap(){
+        let rect2 = this.elem.getBoundingClientRect();
+        return !(boat.x < rect2.left || 
+            boat.x > rect2.right || 
+            boat.y < rect2.top || 
+            boat.y > rect2.bottom);        
+    }
+    move(){
+        this.x -= scrollSpeed;
+        this.elem.style.left = `${this.x}px`;
+        this.elem.style.top = `${this.y}px`;
+
+        if (this.boatOverlap()) {
+            gameOver = true;
+            let sound = new Audio("assets/crash.mp3");
+            sound.play();
+        }
+
+        if (this.x > -20 && !gameOver) {
+            requestAnimationFrame(this.move.bind(this));
+        }
+        else if (!gameOver){
+            this.elem.style.display = "none";
+        }
+    }
+}
+
+class coin extends obj{
+    constructor(x, y){
+        super("assets/coin.png", x, y);
+        this.elem.style.width = `${cw*0.5}px`
+        this.collected = false;
+    }
+    boatOverlap(){
+        let rect1 = boat.elem.getBoundingClientRect();
+        let rect2 = this.elem.getBoundingClientRect();
+        return !(rect1.right < rect2.left || 
+            rect1.left > rect2.right || 
+            rect1.bottom < rect2.top || 
+            rect1.top > rect2.bottom);        
+    }
+    move(){
+        this.x -= scrollSpeed;
+        this.elem.style.left = `${this.x}px`;
+        this.elem.style.top = `${this.y}px`;
+
+        if (this.boatOverlap() && !this.collected) {
+            setTimeout(function(){
+                this.elem.style.display = "none";
+            }.bind(this), 1000);
+            this.elem.style.transform = "translate(-50%, -50%) scale(5)";
+            this.elem.style.opacity = "0";
+            score += 1;
+            document.getElementById("score").innerHTML = score;
+            scrollSpeed += 0.01;
+            this.collected = true;
+            let sound = new Audio("assets/coin.wav");
+            sound.play();
+        }
+
+        if (this.x > -20 && !gameOver) {
+            requestAnimationFrame(this.move.bind(this));
+        }
+        else if (!gameOver){
+            this.elem.style.display = "none";
+        }
+    }
+}
+
+class scylla extends obj {
+    constructor(x, y){
+        super("assets/scylla.gif", x, y);
+        this.elem.style.width = `${cw*0.75}px`;
+        this.direction = 2;
+    }
+    move(){
+        this.x -= scrollSpeed;
+        this.y += this.direction;
+        if (this.y > window.innerHeight-100 || this.y < 100) {
+            this.direction *= -1;
+        }
+        this.elem.style.left = `${this.x}px`;
+        this.elem.style.top = `${this.y}px`;
+
+        if (this.boatOverlap()) {
+            gameOver = true;
+            let sound = new Audio("assets/crash.mp3");
+            sound.play();
+        }
+
+        if (this.x > -20 && !gameOver) {
+            requestAnimationFrame(this.move.bind(this));
+        }
+        else if (!gameOver){
+            this.elem.style.display = "none";
+        }
+    }
+}
+
+function makeCliff() {
+    let r = new obj("assets/cliff.png", window.innerWidth, Math.random()*window.innerHeight);
+    r.move();    
+}
+
+function makeCoin() {
+    let sy = Math.random()*window.innerHeight;
+    for (let i=0; i<3; i++) {
+        let newcoin = new coin(window.innerWidth, sy+i*cw);
+        newcoin.move();
+    }
+}
+
+let columnsSpawned = 0;
+function obstacleSpawner() {
+    if (!gameOver) {
+        if (columnsSpawned % 2 == 0) {
+            makeCliff();
+        }
+        else {
+            makeCoin();
+            if (Math.random() > 0.5) {
+                let s = new scylla(window.innerWidth, Math.random()*window.innerHeight);
+                s.move();
+            }
+        }
+        columnsSpawned += 1;           
+    }
+    else {
+        clearInterval(obsInterval);
+    }
+}
+let obsInterval = setInterval(obstacleSpawner, 700);
+
+function reset() {
+    gameOver = false;
+    score = 0;
+    scrollSpeed = 2;
+    // reinitialize boat
+    boat = new Boat();
+    boat.move();
+    // delete all obstacles
+    document.getElementById("obstacles").innerHTML = "";
+    // enable spawner
+    clearInterval(obsInterval);
+    obsInterval = setInterval(obstacleSpawner, 700);
+    document.getElementById("score").innerHTML = "0";
+}
+onmousedown = reset;
